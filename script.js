6d5a02650f73182efa0f2f83363aeaf728671dba
Danielle Mclean <danielle.mclean@mymona.uwi.edu>
2016-12-06 01:34:19 +0000

    Exercise 4

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-	$("#search").click(function(){
-		var query = $(#query).val();
-		var url = "https://lab6-daniellelala.c9users.io/request.php?q="+query;
-		$.ajax(url, {
+	$("#search").on('click', function(){
+		var query = $("#query").val();
+		var link = "https://lab6-daniellelala.c9users.io/request.php?q="+query;
+		$.ajax(link, {
 			method: 'GET',
 		}).done(function(result){
 			$("#result").html(result);
@@ -10,4 +10,26 @@ $(document).ready(function(){
 			$("#result").html(error);
 		});
 	});	
+	
+	$("#alldefs").on('click', function(){
+        var link = "https://lab6-daniellelala.c9users.io/request.php?q=&all=true";
+        
+        $.ajax(link, {
+        	method: 'GET',
+            dataType: "xml",
+    	}).done(function(res){
+            var defs = $(res).find('definition');
+          
+        	$("#result").append('<ol></ol>');
+           
+            $(defs).each(function(){
+        		var ret = '<h3>'+$(this).attr('name')+'</h3>';
+            	ret += '<p>'+$(this).text()+'</p>';
+            	ret += '<p> -'+$(this).attr('author')+'</p>';
+            	$("#result ol").append('<li>'+ret+'</li>');
+        	});
+    	}).fail(function(err){
+           $("#result").html(err);
+    	});
+	});
 });
